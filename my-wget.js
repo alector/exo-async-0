498be83a9830bc5963774d259491ede047dcd93d
alector <panagiotispp@gmail.com>
2021-04-09 19:24:28 +0200

    adding CLI interface

--- my-wget.js
+++ my-wget.js
@@ -1,14 +1,32 @@
 const fsPromises = require('fs/promises')
 const axios = require('axios')
 var path = require('path')
+let readlineSync = require('readline-sync')
+var validUrl = require('valid-url')
 
-const getHtml = async (url, save_name) => {
-	const saveFileName = `${save_name}.html`
-	const saveFilePath = path.join('downloads', saveFileName)
+const urlValidate = (url) => {
+	if (!validUrl.isUri(url)) {
+		let msg = `⚠️  This doesn't look like a valid URL. \n Please enter a correct url:\n `
+		url = readlineSync.question(msg)
+		return urlValidate(url)
+	}
+
+	return url
+}
+const getHtml = async () => {
+	let msg = 'Please enter a URL to download: '
+	const url = readlineSync.question(msg)
+
+	const verifiedUrl = urlValidate(url)
 
 	try {
-		const response = await axios.get(url)
+		const response = await axios.get(verifiedUrl)
+		const save_name = response.request.socket.servername
+		const saveFileName = `${save_name}.html`
+		const saveFilePath = path.join('downloads', saveFileName)
+
 		await fsPromises.writeFile(saveFilePath, response.data)
+
 		const stats = await fsPromises.stat(saveFilePath)
 		const lengthHeaders = response.headers['content-length']
 
@@ -24,9 +42,10 @@ const getHtml = async (url, save_name) => {
 	}
 }
 
-getHtml('https://www.goodreads.com/author/quotes/903.Homer', 'HomerQuotes')
-
-getHtml(
-	'https://www.successories.com/iquote/author/5/marcus-tullius-cicero-quotes/1',
-	'CiceroQuotes'
-)
+getHtml()
+// getHtml('https://www.goodreads.com/author/quotes/903.Homer', 'HomerQuotes')
+//
+// getHtml(
+// 	'https://www.successories.com/iquote/author/5/marcus-tullius-cicero-quotes/1',
+// 	'CiceroQuotes'
+// )
